0abbe37d1dfc2fdce9a966ea8ecbc0c14f957f46
seyi egunjobi <seyistry@gmail.com>
2021-03-18 10:16:32 +0100

    create homepage with component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import HomePage from "./homepage.component";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+    render() {
+        return (
+            <div className="App">
+                <HomePage></HomePage>
+            </div>
+        );
+    }
 }
 
 export default App;
