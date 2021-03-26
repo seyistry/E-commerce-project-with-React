45215c6fd7111d8a4ff1a14ad243b38f17a9ff86
seyi egunjobi <seyistry@gmail.com>
2021-03-26 14:31:13 +0100

    integrate firbase with the application

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import ShopPage from "./pages/shop/shop.component";
-import Header from './components/header/header.component'
-import SignInAndSignUpPage from './pages/sign-in-sign-up-page/sign-in-sign-up-page.component'
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-sign-up-page/sign-in-sign-up-page.component";
 import HomePage from "./pages/hompages/homepage.component";
+import { auth } from "./firebase/firebase.utils";
 
 const hatsPage = (props) => {
     console.log(props);
@@ -18,14 +19,34 @@ const hatsPage = (props) => {
 };
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentUser: null,
+        };
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+            this.setState({ currentUser: user });
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
     render() {
         return (
             <div className="App">
-                <Header />
+                <Header currentUser={this.state.currentUser}/>
                 <Switch>
                     <Route exact path="/" component={HomePage} />
                     <Route path="/shop" component={ShopPage} />
-                    <Route path='/signin' component={SignInAndSignUpPage} />
+                    <Route path="/signin" component={SignInAndSignUpPage} />
                 </Switch>
             </div>
         );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import { auth } from "../../firebase/firebase.utils";
 import "./header.styles.scss";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
     <div className="header">
-        <Link className='logo-container' to="/">
+        <Link className="logo-container" to="/">
             <Logo className="logo" />
         </Link>
-        <div className='options'>
-            <Link className='options' to='/shop'>
+        <div className="options">
+            <Link className="option" to="/shop">
                 SHOP
             </Link>
-            <Link className='options' to='/shop'>
+            <Link className="option" to="/shop">
                 CONTACT
             </Link>
-
+            {currentUser ? (
+                <div className="option" onclick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+            ) : (
+                <Link className="option" to="/signin">
+                    SIGN IN
+                </Link>
+            )}
         </div>
     </div>
 );
