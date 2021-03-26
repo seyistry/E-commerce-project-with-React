556a428b4805318dfa1f2e217cd37a1be6904195
seyi egunjobi <seyistry@gmail.com>
2021-03-26 13:22:44 +0100

    add google sign in button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,6 +4,7 @@ import FormInput from "../form-input/form-input.component";
 
 import CustomButton from '../custom-button/custom-button.component'
 
+import {signInWithGoogle} from '../../firebase/firebase.utils'
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -53,6 +54,9 @@ class SignIn extends React.Component {
 
                     <CustomButton type="submit" >
                         Sign in
+                    </CustomButton>
+                    <CustomButton onClick={signInWithGoogle} >
+                        Sign in with Google
                     </CustomButton>
                 </form>
             </div>
